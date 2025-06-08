228f14e55ab6f5b51d91705ae6f2f7bec38c1ca7
Luis Garcia <46090720+b3m3bi@users.noreply.github.com>
2025-06-08 13:51:03 -0600

    feat: create functional calculator

--- script.js
+++ script.js
@@ -15,17 +15,10 @@ function divide(num1, num2){
 }
 
 
-const a = 2
-const b = 6
-
-console.log(add(a, b))
-console.log(subtract(a, b))
-console.log(multiply(a, b))
-console.log(divide(a, b))
-
-let num1;
-let num2;
-let operator;
+let num1 = '';
+let num2 = '';
+let operator = '';
+let error = false;
 
 function operate(operator, num1, num2){
     switch (operator) {
@@ -40,7 +33,83 @@ function operate(operator, num1, num2){
     }
 }
 
-console.log(operate("+", a, b))
-console.log(operate("-", a, b))
-console.log(operate("*", a, b))
-console.log(operate("/", a, b))
+const btnsContainer = document.querySelector('.btns-container');
+const displayCapture = document.querySelector('#capture');
+const operators = ['+', '*', '-', '/'];
+
+btnsContainer.addEventListener('click', (event) => {
+    if (event.target.classList.contains('btn')){
+        let clickedValue = event.target.textContent;
+        console.log(clickedValue);
+
+        if (clickedValue === 'AC'){
+            resetMemory();
+            displayCapture.textContent = '';
+            error = false;
+        } else if (clickedValue === 'DEL' && !error){
+            let updatedValue = displayCapture.textContent.slice(0, -1);
+            displayCapture.textContent = updatedValue;
+
+        } else if (operators.includes(clickedValue)  && !error) {
+
+            if (displayCapture.textContent){
+                // If there is any number in display...
+                if (!operator){
+                    // If there is not an operator in memory yet...
+                    // Save number in display and operator in memory 
+                    operator = clickedValue;
+                    num1 = displayCapture.textContent.trim();
+
+                    displayCapture.textContent = '';
+                } else {
+                    // If there is already an operator in memory...
+                    // Make operation in memory using number in display
+                    // as second number, and display result
+                    // Save result and new operator in memory
+                    let resultPreviousOperaton = getResultMermoryOperation();
+                    displayCapture.textContent = resultPreviousOperaton;
+
+                    num1 = resultPreviousOperaton;
+                    operator = clickedValue;
+                }
+            }
+        } else if (clickedValue === '='  && !error){
+            if (operator && displayCapture.textContent){
+                // If there was an operator and second number, operate...
+                // Remove numbers and operators form memory...
+                let result = getResultMermoryOperation();
+                displayCapture.textContent = result;
+                
+                resetMemory();
+    
+            }
+        } else if (!error) {
+            if (operator && num2){
+                displayCapture.textContent = '';
+            } 
+            // Number of digits can't be greater than 13 to avoid
+            // overflow from display
+            if (displayCapture.textContent.trim().length <= 10){
+                displayCapture.textContent = displayCapture.textContent + clickedValue;
+            }
+        }
+    }
+
+})
+
+
+function resetMemory(){
+    num1 = '';
+    num2 = '';
+    operator = '';
+}
+
+function getResultMermoryOperation(){
+    num2 = displayCapture.textContent.trim();
+    let result = operate(operator, +num1, +num2);
+    if (result === Infinity || !result){
+        result = "ERROR";
+        error = true;
+    }
+    return result;
+}
